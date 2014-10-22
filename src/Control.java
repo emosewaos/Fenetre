b7defa86a742bac050ca730a58c2a6cdd1d6626c
VDB <I4maluckbox@yahoo.com>
2014-10-22 09:22:00 +0200

    Projet d'une fenetre

---
+++ src/Control.java
@@ -0,0 +1,8 @@
+
+public class Control {
+	public static void main(String[] args){
+		
+		Fenetre fen = new Fenetre();
+	}
+	
+}
---
+++ src/Panneau.java
@@ -0,0 +1,8 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+ 
+public class Panneau extends JPanel {
+  public void paintComponent(Graphics g){
+    g.drawString("dafuq", 10, 20);
+  }               
+}
